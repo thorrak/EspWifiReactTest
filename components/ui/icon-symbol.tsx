147ba8dc46ca788eca24990c451465b24fa9e151
Thorrak <thorrak@excelinspace.com>
2026-02-21 22:27:10 -0500

    Fix icon for Android

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -18,6 +18,7 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'antenna.radiowaves.left.and.right': 'settings-input-antenna',
 } as IconMapping;
 
 /**
